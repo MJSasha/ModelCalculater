968fc4f02e9c74b89a19dd49f813436b777a2d62
MJSasha <sasha.ushankov@gmail.com>
2023-05-16 21:28:43 +0300

    Refactor

--- UI/Pages/MatrixPage.razor.cs
+++ UI/Pages/MatrixPage.razor.cs
@@ -28,16 +28,16 @@ namespace UI.Pages
 
         public void Dispose()
         {
-            MatrixActionsService.OnClearPressed -= ClearMatrix;
+            MatrixActionsService.OnClearPressed -= OnClearPressed;
             MatrixActionsService.OnRedactionPressed -= OnRedactButtonPressed;
-            MatrixActionsService.OnCalculatePressed -= CalculateMatrix;
+            MatrixActionsService.OnCalculatePressed -= OnCalculatePressed;
         }
 
         protected override void OnInitialized()
         {
-            MatrixActionsService.OnClearPressed += ClearMatrix;
+            MatrixActionsService.OnClearPressed += OnClearPressed;
             MatrixActionsService.OnRedactionPressed += OnRedactButtonPressed;
-            MatrixActionsService.OnCalculatePressed += CalculateMatrix;
+            MatrixActionsService.OnCalculatePressed += OnCalculatePressed;
         }
 
         private void AddRow()
@@ -47,63 +47,6 @@ namespace UI.Pages
                 col.Value.Add(0);
             }
             StateHasChanged();
-        }
-
-        private void ClearMatrix()
-        {
-            matrix = new();
-            definedVariables = new();
-            requiredVariables = new();
-            isRedacted = false;
-            StateHasChanged();
-        }
-
-        private void OnRedactButtonPressed()
-        {
-            isRedacted = !isRedacted;
-            StateHasChanged();
-        }
-
-        private void RemoveColumn(string columnName)
-        {
-            matrix.Remove(columnName);
-            definedVariables.Remove(columnName);
-            requiredVariables.Remove(columnName);
-            if (matrix.Count == 0) isRedacted = false;
-            StateHasChanged();
-        }
-
-        private void RemoveRow(int rowIndex)
-        {
-            foreach (var col in matrix)
-            {
-                col.Value.RemoveAt(rowIndex);
-            }
-        }
-
-        private void ChangeValue(string columnName, int index)
-        {
-            matrix[columnName][index] = matrix[columnName][index] > 0 ? 0 : 1;
-            StateHasChanged();
-        }
-
-        private void OnColumnPressed(string columnName)
-        {
-            if (requiredVariables.Contains(columnName)) requiredVariables.Remove(columnName);
-            else if (definedVariables.Contains(columnName))
-            {
-                definedVariables.Remove(columnName);
-                requiredVariables.Add(columnName);
-            }
-            else definedVariables.Add(columnName);
-            StateHasChanged();
-        }
-
-        private string GetColumnColor(string columnName)
-        {
-            if (requiredVariables.Contains(columnName)) return "background-color: var(--bs-success); color: var(--bs-white)";
-            else if (definedVariables.Contains(columnName)) return "background-color: var(--bs-danger); color: var(--bs-white)";
-            else return "";
         }
 
         private async void AddColumn()
@@ -130,7 +73,37 @@ namespace UI.Pages
             catch { /*ignore*/ }
         }
 
-        private async void CalculateMatrix()
+        private void RemoveColumn(string columnName)
+        {
+            matrix.Remove(columnName);
+            definedVariables.Remove(columnName);
+            requiredVariables.Remove(columnName);
+            if (matrix.Count == 0) isRedacted = false;
+            StateHasChanged();
+        }
+
+        private void RemoveRow(int rowIndex)
+        {
+            foreach (var col in matrix)
+            {
+                col.Value.RemoveAt(rowIndex);
+            }
+        }
+
+        private void ChangeValue(string columnName, int index)
+        {
+            matrix[columnName][index] = matrix[columnName][index] > 0 ? 0 : 1;
+            StateHasChanged();
+        }
+
+        private string GetColumnColor(string columnName)
+        {
+            if (requiredVariables.Contains(columnName)) return "background-color: var(--bs-success); color: var(--bs-white)";
+            else if (definedVariables.Contains(columnName)) return "background-color: var(--bs-danger); color: var(--bs-white)";
+            else return "";
+        }
+
+        private async void OnCalculatePressed()
         {
             var matrixWithoutDefineVariables = new Matrix(matrix.Where(m => !definedVariables.Contains(m.Key)).ToDictionary(s => s.Key, s => s.Value));
 
@@ -154,6 +127,33 @@ namespace UI.Pages
                 }
                 catch (Exception) { /*ignore*/ }
             }
+        }
+
+        private void OnRedactButtonPressed()
+        {
+            isRedacted = !isRedacted;
+            StateHasChanged();
+        }
+
+        private void OnColumnPressed(string columnName)
+        {
+            if (requiredVariables.Contains(columnName)) requiredVariables.Remove(columnName);
+            else if (definedVariables.Contains(columnName))
+            {
+                definedVariables.Remove(columnName);
+                requiredVariables.Add(columnName);
+            }
+            else definedVariables.Add(columnName);
+            StateHasChanged();
+        }
+
+        private void OnClearPressed()
+        {
+            matrix = new();
+            definedVariables = new();
+            requiredVariables = new();
+            isRedacted = false;
+            StateHasChanged();
         }
     }
 }
